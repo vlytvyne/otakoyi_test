acf691232ea54f017c6d16e4a5a720102ebfc5dd
Vadym Lytvynets <v.lytvynets61@gmail.com>
2021-02-06 13:56:58 +0200

    init and setup repo

---
+++ android/app/src/main/kotlin/com/otakoyi_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.otakoyi_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/other/AssetProvider.dart
@@ -0,0 +1,9 @@
+class AssetProvider {
+  
+  static String getIcon(String iconName) =>
+      'assets/icons/' + iconName;
+  
+  static String getImage(String imageName) =>
+      'assets/images/' + imageName;
+  
+}
